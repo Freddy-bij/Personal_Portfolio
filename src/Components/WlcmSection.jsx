e31b6fbc6759b41df2cb7043f3b5b70eb315197a
Freddy Bijanja <freddybijanja31@gmail.com>
2025-02-06 09:25:59 +0200

    project-setup

---
+++ src/Components/WlcmSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const WlcmSection = () => {
+  return (
+    <div className='bg-[url("./public/images/bac-remove.png")] h-screen w-full bg-no-repeat bg-cover py-6 '>
+        <NavBar />
+        
+    </div>
+  )
+}
+
+export default WlcmSection
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import WlcmSection from "../Components/WlcmSection"
+
+
+const Home = () => {
+  return (
+    <div className="bg-black h-screen text-white">
+        <WlcmSection/>
+        
+    </div>
+  )
+}
+
+export default Home
