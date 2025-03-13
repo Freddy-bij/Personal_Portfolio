0166d5fc0029089137b45f1051cd797534f7e1a8
Freddy Bijanja <freddybijanja31@gmail.com>
2025-03-13 12:48:42 +0200

    chore: add show more and less about section

--- src/Components/SectAbout.jsx
+++ src/Components/SectAbout.jsx
@@ -1,7 +1,16 @@
 import { FaGithub } from "react-icons/fa6"
 import freddy from "../images/freddy prof.png"
 import { Link } from "react-router"
+import { useState } from "react"
+
 const SectAbout = () => {
+  const [chowAllParagraph,setChowAllParagraph]=useState(false) 
+
+  let paragraph = "I am a passionate full stack developer with a degree in Business Management and Computer Science with 1 year of web development training in front end and backend. My expertise lies in leveraging cutting edge technologies such as React JS, node js and various UI frameworks including, Tailwind css, Chachra UI and antdesign. My ability also lies in designing seamless UIs and components that align perfectly with the desired aesthetics, creating responsive websites that gracefully adapt to any device. My skills also extend to creating the data in the competent database. ensuring the seamless flow of data between the front-end and the back-end. Moreover, I write the code in a clean and well-structured manner that not only works flawlessly but also improves the overall maintainability of the projects."
+
+  if(!chowAllParagraph){
+    paragraph = paragraph.substring(0,515) + '...'
+}
   return (
     <div className=" w-[70%]  font-[sofia] mx-auto bg-[url('./public/images/imgremov.png')] bg-no-repeat bg-center sm:bg-none ">
 
@@ -9,7 +18,10 @@ const SectAbout = () => {
         <div >
           <div >
             <h1 className="text-[#ff3f34] text-2xl uppercase font-bold  pb-3 md:text-start text-center " >About Me</h1>
-            <p className=" text-xl ">I am a passionate full stack developer with a degree in Business Management and Computer Science with 1 year of web development training in front end and backend. My expertise lies in leveraging cutting edge technologies such as React JS, node js and various UI frameworks including, Tailwind css, Chachra UI and antdesign. My ability also lies in designing seamless UIs and components that align perfectly with the desired aesthetics, creating responsive websites that gracefully adapt to any device. My skills also extend to creating the data in the competent database. ensuring the seamless flow of data between the front-end and the back-end. Moreover, I write the code in a clean and well-structured manner that not only works flawlessly but also improves the overall maintainability of the projects..</p>
+            
+                          <p className=" text-xl ">{paragraph} <i onClick={()=> setChowAllParagraph((prevChowAllParagraph) => !prevChowAllParagraph)} className="text-[#ff3f34]/40">{!chowAllParagraph ? "more" : "less"}</i> </p>
+
+            
           </div>
 
         </div>
