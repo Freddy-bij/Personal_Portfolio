e0ae76bdce6a4968467bc7c35e30bfb97979f24a
Freddy Bijanja <freddybijanja31@gmail.com>
2025-04-10 14:17:57 +0200

    improve About section design

--- src/Components/SectAbout.jsx
+++ src/Components/SectAbout.jsx
@@ -1,23 +1,29 @@
-import { FaGithub } from "react-icons/fa6"
+import { FaArrowUpRightFromSquare, FaGithub } from "react-icons/fa6"
 import freddy from "../images/freddy prof.png"
 import { Link } from "react-router"
-import { useState } from "react"
+
 
 const SectAbout = () => {
-  const [chowAllParagraph, setChowAllParagraph] = useState(false)
 
-  let paragraph = "I am a passionate full stack developer with a degree in Business Management and Computer Science with 1 year of web development training in front end and backend. My expertise lies in leveraging cutting edge technologies such as React JS, node js and various UI frameworks including, Tailwind css, Chachra UI and antdesign. My ability also lies in designing seamless UIs and components that align perfectly with the desired aesthetics, creating responsive websites that gracefully adapt to any device. My skills also extend to creating the data in the competent database. ensuring the seamless flow of data between the front-end and the back-end. Moreover, I write the code in a clean and well-structured manner that not only works flawlessly but also improves the overall maintainability of the projects."
-
-  if (!chowAllParagraph) {
-    paragraph = paragraph.substring(0, 515) + '...'
-  }
   return (
     <div className=" sm:w-[70%]  font-[sofia] sm:mx-auto mx-4  ">
+
       <div className="grid xl:grid-cols-2 mt-20   p-2 rounded-sm sm:bg-none ">
         <div >
           <div >
             <h1 className="text-[#ff3f34] text-2xl uppercase font-bold  pb-3 md:text-start text-center " >About Me</h1>
-            <p className=" text-xl ">{paragraph} <i onClick={() => setChowAllParagraph((prevChowAllParagraph) => !prevChowAllParagraph)} className="text-[#ff3f34]/40">{!chowAllParagraph ? "more" : "less"}</i> </p>
+
+            <div>
+              <p className="text-xl relative">I am a passionate full stack developer with a degree in Business Management and Computer Science
+                with 1 year of web development training in front end and backend. My expertise lies in leveraging cutting edge technologies such as React JS, n
+                ode js and various UI frameworks including, Tailwind css, Chachra UI and antdesign ...<Link to="about" target="_blank"><span className="absolute text-sm letf-10 bottom-[5px] ml-1 text-[#ff3f34] hover:text-white"><FaArrowUpRightFromSquare /></span></Link> </p>
+
+              <div className="flex gap-4 mt-3">
+                <Link to="/contact" target="_blank"><button className="border px-3 py-1 rounded-sm font-bold text-black shadow-sm bg-white hover:text-white hover:border  hover:border-gray-900 hover:bg-black">Hire me</button></Link>
+                <Link to="https://github.com/Freddy-bij" target="_blank"> <button className="bg-[#ff3f34] px-3 py-1 rounded-sm font-bold text-black  hover:border  hover:border-gray-900 hover:bg-black flex items-center gap-1 hover:text-white  "> <FaGithub /> Find me</button></Link>
+              </div>
+            </div>
+
           </div>
         </div>
         <div className=" flex justify-center">
@@ -26,10 +32,7 @@ const SectAbout = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-4 mt-3">
-        <Link to="/contact" target="_blank"><button className="border px-3 py-1 rounded-sm font-bold hover:bg-[#ff3f34] hover-border-none">Hire me</button></Link>
-        <Link to="https://github.com/Freddy-bij" target="_blank"> <button className="bg-[#ff3f34] px-3 py-1 rounded-sm font-bold text-black  hover:border hover:bg-black flex items-center gap-1 hover:text-white  "> <FaGithub /> Find me</button></Link>
-      </div>
+
     </div>
   )
 }
