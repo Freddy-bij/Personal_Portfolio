66ca34e86320297e7e428863d4b53658c9c257a9
Freddy bijanja <freddybijanja31@gmail.com>
2025-12-11 15:19:20 +0300

    Fix formatting and import path in Contact.route.js

--- backend/routes/Contact.route.js
+++ backend/routes/Contact.route.js
@@ -1,10 +1,11 @@
 
-const express = require("express")
-const  SendEmailMessage = require("../controller/Contact.controller.js")
+const express = require("express");
+const SendEmailMessage = require("../controller/Contact.controller");
 
 const router = express.Router();
 
-router.post ("/contact" , SendEmailMessage)
+router.post("/contact", SendEmailMessage);
 
-module.exports = router
+module.exports = router;
+
 
