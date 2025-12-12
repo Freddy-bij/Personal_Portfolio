2989456dd00f33fa0fbb6112fa09f8b51fbcc87d
Freddy Bijanja <freddybijanja31@gmail.com>
2025-12-12 17:54:15 +0200

    add render link url

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config(); 
+const dotenv = require("dotenv");
+const router = require("./routes/Contact.route.js");
 
-const router = require("./routes/Contact.route");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
 
 app.use(cors({
   origin: [
@@ -18,30 +18,15 @@ app.use(cors({
   credentials: true
 }));
 
-
+app.options("*", cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use("/api", router);
+app.use("/api/contact", router);
 
 app.get("/", (req, res) => {
-  res.json("Portfolio api is running");
+  res.send("Backend is running...");
 });
-
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: "Something went wrong"
-  });
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
