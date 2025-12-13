b1ddfea69fee998f9d51373f4451769c5771bf48
Freddy Bijanja <freddybijanja31@gmail.com>
2025-12-13 18:40:28 +0200

    Fix: Switching to explicit SMTP host/port 587 (STARTTLS) to resolve final Connection Timeout

--- backend/controller/Contact.controller.js
+++ backend/controller/Contact.controller.js
@@ -32,7 +32,8 @@ const SendEmailMessage = async (req, res) => {
       // service: "gmail",
       host: "smtp.gmail.com", 
       port: 465,             
-      secure: true,
+      secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
